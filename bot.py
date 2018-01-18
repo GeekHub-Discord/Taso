a7f5c75bbec7b1bdb429206eeb18b7262993c26c
Jacob Wiltse <kelwing@kelnet.org>
2018-01-18 15:39:10 -0500

    perms handling take 2

--- bot.py
+++ bot.py
@@ -15,7 +15,9 @@ class Bot():
 
     async def call(self, command, message):
         handler = self.commands.get(command)
-        if handler: # and message.author.server_permissions.is_subset(handler[1]):
-            return await handler[0](message)
+        if handler:
+            if message.author.server_permissions >= handler[1]:
+                return await handler[0](message)
+            raise ValueError(f"Not enough permissions for {command}")
         else:
             raise ValueError(f'Command "{command}" has is not registered')
