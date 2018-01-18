dff110659632d9dac43becb53eede6a534ab9b5f
Jacob Wiltse <kelwing@kelnet.org>
2018-01-18 16:07:25 -0500

    Fixed peewee query

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ async def on_message(message):
                     try:
                         role = Role.get(Role.awardlevel == level)
                         lastrole = Role.select().where(
-                            Role.server == server and Role.awardlevel is not None).order_by(
+                            Role.server == server and Role.awardlevel.is_null(False)).order_by(
                                 Role.awardlevel.desc()
                             ).limit(1)
                         if len(lastrole) > 0:
